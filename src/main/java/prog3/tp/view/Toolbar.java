e14d68e02d6a4dd3982b84fb6de25fb6496db487
RomeGL <lautaaromeroo@gmail.com>
2026-05-11 21:02:10 -0300

    feat: added the button to start the program

--- src/main/java/prog3/tp/view/Toolbar.java
+++ src/main/java/prog3/tp/view/Toolbar.java
@@ -1,5 +1,6 @@
 package prog3.tp.view;
 
+import javax.swing.Box;
 import javax.swing.JOptionPane;
 import javax.swing.JToolBar;
 import java.awt.event.ActionListener;
@@ -9,6 +10,7 @@ class Toolbar extends JToolBar {
     private ToolbarButton _localityButton;
     private ToolbarButton _connectionsButton;
     private ToolbarButton _helpButton;
+    private ToolbarButton _startButton;
     private ToolbarListener _listener;
 
     public Toolbar(ToolbarListener listener) {
@@ -21,14 +23,17 @@ class Toolbar extends JToolBar {
         initLocalityButton();
         initConnectionsButton();
         initHelpButton();
+        initStartButton();
 
         this.add(_localityButton);
         this.add(_connectionsButton);
         this.addSeparator();
         this.add(_helpButton);
+        this.add(Box.createHorizontalGlue());
+        this.add(_startButton);
     }
 
-    private void initLocalityButton() {
+	private void initLocalityButton() {
         _localityButton = new ToolbarButton("");
         _localityButton.setToolTipText("Add new locality.");
         _localityButton.addActionListener(new ActionListener() {
@@ -93,4 +98,16 @@ class Toolbar extends JToolBar {
                 null,
                 null);
    }
+    
+    private void initStartButton() {
+    	 _startButton = new ToolbarButton("✓");
+         _startButton.setToolTipText("Show in the map the localities connections and the cost");
+         _startButton.addActionListener(
+                 new ActionListener() {
+                     @Override
+                     public void actionPerformed(ActionEvent e) {
+                         //need create the start method to connect all the localities and show the cost.
+                     }
+                 });
+	}
 }
