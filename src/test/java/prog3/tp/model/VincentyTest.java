1120f0ac1bf0cf898085c8387a252ca0e2aa401d
Emiliano-Carrasco <emilianocarrascogold@gmail.com>
2026-05-12 18:33:41 -0300

    nuevos test de Vicenty

--- src/test/java/prog3/tp/model/VincentyTest.java
+++ src/test/java/prog3/tp/model/VincentyTest.java
@@ -61,4 +61,44 @@ public class VincentyTest {
             Vincenty.distance(lat1, lon1, lat2, lon2);
         });
     }
+    
+    @Test
+    public void testNorthPoleToSouthPole() {
+       
+    	 // A lo largo de un meridiano, del polo norte al sur
+        double lat1 = 90;
+        double lon1 = 0;
+
+        double lat2 = -90;
+        double lon2 = 0;
+        
+        double distance = Vincenty.distance(lat1, lon1, lat2, lon2);
+        
+        // Aproximadamente 20,003.93 km
+        assertEquals(20003.93, distance, 0.1);
+    }
+    
+    @Test
+    public void testEquatorialDistance() {
+        // Dos puntos en el ecuador separados por 1 grado
+        double lat1 = 0;
+        double lon1 = 0;
+
+        double lat2 = 0;
+        double lon2 = 1;
+        
+        double distance = Vincenty.distance(lat1, lon1, lat2, lon2);
+
+        // 111.319 km aprox
+        assertEquals(111.319, distance, 0.001);
+    }
+    
+    @Test
+    public void testLongitudeNormalization() {
+        // Debería ser la misma distancia que de 0 a 10
+        double dist1 = Vincenty.distance(0, 0, 0, -10);
+        double dist2 = Vincenty.distance(0, 350, 0, 0); // 350 es lo mismo que -10
+        
+        assertEquals(dist1, dist2, 0.001);
+    }
 }
